99c73c806e420c0424b36fcb5fd3e6c3dc4018a5
wojiwo <wojiwo03@outlook.com>
2021-06-06 20:51:36 +0800

    增加单元测试依赖

--- goods-service/src/main/java/com/wojiwo/goods/controller/TbSpecParamController.java
+++ goods-service/src/main/java/com/wojiwo/goods/controller/TbSpecParamController.java
@@ -4,6 +4,7 @@ import com.wojiwo.goods.entity.TbSpecParam;
 import com.wojiwo.goods.service.TbSpecParamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,8 +14,8 @@ public class TbSpecParamController {
     @Autowired
     TbSpecParamService tbSpecParamService;
 
-    @PostMapping(value = "/specParam",params = {"ids"})
-    public List<TbSpecParam> getByIds(List<String> ids) {
+    @PostMapping(value = "/specParam/ids")
+    public List<TbSpecParam> getByIds(@RequestBody List<Integer> ids) {
         return tbSpecParamService.listByIds(ids);
     }
 }
